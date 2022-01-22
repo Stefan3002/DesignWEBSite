550c54a5b50ce14092277dfc1d08877f9f2ea56f
Stefan3002 <92794548+Stefan3002@users.noreply.github.com>
2022-01-22 22:13:36 +0200

    Added Extended Footer and JS.

---
+++ JavaScript/script.js
@@ -0,0 +1,38 @@
+function ExtendFooter() {
+    console.log("dsadas")
+    const content = `
+    <div class="ExtendedFooterItem">
+    <img class="Left" src="../Images/SocialMedia%20Icons/Facebook.png">
+    </div>
+    <div class="ExtendedFooterItem">
+    <img class="MiddleUp" src="../Images/SocialMedia%20Icons/Twitter.png">
+    </div>
+    <div class="ExtendedFooterItem">
+    <img class="MiddlePopUp" src="../Images/SocialMedia%20Icons/Instagram.png">
+    </div>
+    <div class="ExtendedFooterItem">
+    <img class="MiddleDown" src="../Images/SocialMedia%20Icons/Mail.png">
+    </div>
+    <div class="ExtendedFooterItem">
+    <img class="Right" src="../Images/SocialMedia%20Icons/LinkedIn.png">
+    </div>
+    <img class="Right" onclick="RemoveFooter()" src="../Images/SocialMedia%20Icons/Minus.png">
+    
+`
+
+    if(!(document.querySelector("#ExtendedFooter"))){
+        const element = document.createElement("section")
+        element.id = "ExtendedFooter"
+        const Footer = document.querySelector("#ExtendedFooterContainer")
+        element.innerHTML = content
+        const plusButton = document.querySelector(".SeeMore")
+        // plusButton.setAttribute("src","Images/SocialMedia%20Icons/Minus.png")
+        Footer.appendChild(element)
+    }
+}
+function RemoveFooter(){
+    const ExtendedFooter = document.querySelector("#ExtendedFooter")
+    if(ExtendedFooter)
+        ExtendedFooter.remove()
+
+}
